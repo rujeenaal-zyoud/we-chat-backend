7779b65b40bc6c933a7631aad375685499a01406
Haneenabonser <aabonser@gmail.com>
2021-07-13 17:31:45 +0300

    haneen

--- models/User.js
+++ models/User.js
@@ -5,75 +5,68 @@ const jwt = require('jsonwebtoken');
 const SECRET = 'supersecret';
 
 const userSchema = new mongoose.Schema(
-    {
-        username : {
-            type :String ,
-              required:true ,
-              
+  {
+    username: {
+      type: String,
+      required: true,
 
-        },
-        email :{
-            type :String ,
-            required:true ,
-            unique:true
-        },
-
-        password: {
-            type: String,
-            required:true ,
-
-        }
+    },
+    email: {
+      type: String,
      
+    },
+
+    password: {
+      type: String,
+      required: true,
 
     }
+  }
 );
 
-
-
-
 userSchema.virtual('token').get(function () {
-    let tokenObject = {
-      username: this.username,
-    
-    };
-    return jwt.sign(tokenObject,SECRET,{expiresIn: '15m'});
-  });
-  
-  userSchema.pre('save', async function () {
-    if (this.isModified('password')) {
-      this.password = await bcrypt.hash(this.password, 10);
-    }
-  });
-  
-  
-  // BASIC AUTH
-  userSchema.statics.authenticateBasic = async function (username,password) {
-      const user = await this.findOne({ username })
-    //   const email = await this.findOne({email})
-      const valid = await bcrypt.compare(password, user.password)
-      if (valid) { return user; }
-      throw new Error('Invalid User');
-    }
-  
-  
-  
-  // BEARER AUTH
-  userSchema.statics.authenticateWithToken = async function (token) {
-    try {
-      const parsedToken = jwt.verify(token,SECRET);
-      const user = await this.findOne({ username: parsedToken.username });
-      if (user) { return user; }
-      else{
-        throw new Error('User Not Found');
-      }
-    
-    } catch (e) {
-      throw new Error(e.message);
-    }
-  };
-  
-  
-  
-  
+  let tokenObject = {
+    username: this.username,
 
-module.exports=mongoose.model("User",userSchema);
+  };
+  return jwt.sign(tokenObject, SECRET, { expiresIn: '15m' });
+});
+
+userSchema.pre('save', async function () {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+});
+
+
+// BASIC AUTH
+userSchema.statics.authenticateBasic = async function (username, password) {
+  const user = await this.findOne({ username })
+  //   const email = await this.findOne({email})
+  const valid = await bcrypt.compare(password, user.password)
+  if (valid) { return user; }
+  throw new Error('Invalid User');
+}
+
+
+
+// BEARER AUTH
+userSchema.statics.authenticateWithToken = async function (token) {
+  try {
+    const parsedToken = jwt.verify(token, SECRET);
+    const user = await this.findOne({ username: parsedToken.username });
+    if (user) { return user; }
+    else {
+      throw new Error('User Not Found');
+    }
+
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
+
+
+
+
+
+module.exports = mongoose.model("User", userSchema);
--- server.js
+++ server.js
@@ -40,91 +40,66 @@ const getUser = (userId) => {
 };
 
 
-//1 
-//io.on ==> send from server to client
-
-io.on("connection", (socket) => {
-  //when ceonnect
-  console.log("a user connected.");
-  io.emit("Hello from server socket");
-  //===> then go to frontend  and take the event from here using .on 
-  // and we don't know each socket id to send for each one becuse it change
-  //So how we will send message to each one ?
-  //I will create array  and 
-  //3 ==> go to front end 
-  //In front end .emmit create the user_id  "addUser"==from front end 
-  //take userId and socketId from user
-  socket.on("addUser", (userId) => {
-    // here i add all user in front end with .emmit and send  to take it here 
-    addUser(userId, socket.id);
-    // then i want to get it from here and send to front with .on
-    io.emit("getUsers", users);
-  });
-
-
-  //6 send and get message will send from front 
-  //sender=user
-  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-    // get the user that we specily want to send to
-    const user = getUser(receiverId);
-    // send the message 
-    io.to(user.socketId).emit("getMessage", {
-      //thr meassage content
-      senderId,
-      text,
-    });
-  });
-  // ==> go to front 
-
-
-  //5   create function for remove user 
-  //socketId that created each time when add 
-  const removeUser = (userId) => {
-    users = users.filter(user => user.socketId !== socketId)
+io.use(async (socket, next) => {
+  try {
+    const token = socket.handshake.query.token;
+    const payload = await jwt.verify(token, process.env.SECRET); socket.userId = payload.id; next();
   }
-  //4
-  //when disconnect we shoud remove a user from array
-  socket.on("disconnect", () => {
-    console.log("a user disconnected!");
-    removeUser(socket.id);
-    //send to all the new array 
-    io.emit("getUsers", users);
-  });
+  catch (err) { }
 });
+io.on("connection", (socket) => {
+  console.log("Connected: " + socket.userId);
+  socket.on("disconnect", () => { console.log("Disconnected: " + socket.userId); });
+})
 
-
-
-
-// //create socket for token 
-// io.use (async(socket,next)=>{
-//  //from   https://www.tabnine.com/code/javascript/functions/socket.io/Handshake/address
-//         const token = socket.handshake.query.token;
-//         const payload = await jwt.verify(token, process.env.SECRET);
-//         //for use payload each time i put it in userId
-//         socket.userId = payload.id;
-//         next();
-
-// }) 
+// //1 
+// //io.on ==> send from server to client
 
 // io.on("connection", (socket) => {
-//     console.log("Connected: " + socket.userId);
-//   //هون لازم اجيب ال يوزر من داتا بيس ؟؟ واعمل emit
-//     });
-//       //send and get message
+//   //when ceonnect
+//   console.log("a user connected.");
+//   io.emit("Hello from server socket");
+//   //===> then go to frontend  and take the event from here using .on 
+//   // and we don't know each socket id to send for each one becuse it change
+//   //So how we will send message to each one ?
+//   //I will create array  and 
+//   //3 ==> go to front end 
+//   //In front end .emmit create the user_id  "addUser"==from front end 
+//   //take userId and socketId from user
+//   socket.on("addUser", (userId) => {
+//     // here i add all user in front end with .emmit and send  to take it here 
+//     addUser(userId, socket.id);
+//     // then i want to get it from here and send to front with .on
+//     io.emit("getUsers", users);
+//   });
+
+
+//   //6 send and get message will send from front 
+//   //sender=user
 //   socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+//     // get the user that we specily want to send to
 //     const user = getUser(receiverId);
+//     // send the message 
 //     io.to(user.socketId).emit("getMessage", {
+//       //thr meassage content
 //       senderId,
 //       text,
 //     });
 //   });
+//   // ==> go to front 
 
 
-
-
-
-
-
-
-
-
+//   //5   create function for remove user 
+//   //socketId that created each time when add 
+//   const removeUser = (userId) => {
+//     users = users.filter(user => user.socketId !== socketId)
+//   }
+//   //4
+//   //when disconnect we shoud remove a user from array
+//   socket.on("disconnect", () => {
+//     console.log("a user disconnected!");
+//     removeUser(socket.id);
+//     //send to all the new array 
+//     io.emit("getUsers", users);
+//   });
+// });
